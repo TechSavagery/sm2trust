5224341dd0add9ee7740b50d420f4df4b16bafc8
ladellerby <ladellerby@gmail.com>
2024-09-03 17:19:23 -0700

    - update sitemap to be dynamically generated

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         unoptimized: true
     },
     
-    output: "export",
+    //output: "export",
 };
 
 export default nextConfig;
---
+++ src/app/sitemap.js
@@ -0,0 +1,46 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://sm2trust.com',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+      {
+        url: 'https://sm2trust.com/#about',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.8,
+      },
+      {
+        url: 'https://sm2trust.com/#services',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.7,
+      },
+      {
+        url: 'https://sm2trust.com/contact',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.6,
+      },
+      {
+        url: 'https://sm2trust.com/#faq',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.5,
+      },
+      {
+        url: 'https://sm2trust.com/#team',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.4,
+      },
+      {
+        url: 'https://sm2trust.com/#blog',
+        lastModified: new Date(),
+        changeFrequency: 'weekly',
+        priority: 0.3,
+      },
+    ]
+  }
